57d0bc9d4a9ff2a22539523a614427a75a16becc
Gun-93 <gunpcity@gmail.com>
2025-08-15 22:25:41 +0530

    Prepare for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/React_Portfolio/',
+  
   plugins: [
     react(),
     tailwindcss(),
